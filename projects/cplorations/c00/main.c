59f3bd3d9b3e48572c868c1bead7395183d4b24f
Nick Nyburg <nick@Mac.lan>
2025-09-28 15:35:12 -0700

    Completed cploration 0 Hello World Program

---
+++ projects/cplorations/c00/main.c
@@ -0,0 +1,12 @@
+/************************************
+ * C-ploration 0 for CS 271
+ * Nick Nyburg
+ * Fall 2025
+ ************************************/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
