2f4fdf6cbe247bb696d8e4c70a7e311eb8cce32a
Nick Nyburg <nick@Mac.lan>
2025-10-20 22:13:26 -0700

    Set up basic project structure for c ploration 3

---
+++ projects/cplorations/c03/main.c
@@ -0,0 +1,18 @@
+/****************************************
+ * C-ploration 3 for CS 271
+ * 
+ * Nick Nyburg
+ * Fall 2025
+ * 
+ ****************************************/
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 200
+
+int main()
+{	
+	return 1;
+}
